3473ca3c01dec136bb1623eab303c8693b6a26e8
Thomas J Faughnan Jr <thomas@faughnan.net>
2024-12-05 18:16:07 -0500

    day05 part 2: use a closure for cmp
    This avoids needing `rules` to be global

--- day05/2.py
+++ day05/2.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from collections.abc import Callable
 import functools
 import itertools
 import sys
@@ -9,13 +10,15 @@ def is_correctly_ordered(rules: defaultdict[int, list[int]], update: list[int]) 
             return False
     return True
 
-# XXX: relies on rules being global (cmp_to_key functions take exactly 2 args)
-def cmp(x: int, y: int) -> int:
-    if x in rules[y]:
-        return 1
-    elif y in rules[x]:
-        return -1
-    return 0
+def cmp_with_rules(rules: defaultdict[int, list[int]]) -> Callable[[int, int], int]:
+    def cmp(x: int, y: int) -> int:
+        if x in rules[y]:
+            return 1
+        elif y in rules[x]:
+            return -1
+        return 0
+
+    return cmp
 
 rules_input, updates_input = sys.stdin.read().split('\n\n')
 rules = defaultdict[int, list[int]](list)
@@ -27,7 +30,7 @@ total = 0
 for line in updates_input.strip().split('\n'):
     update = [*map(int, line.split(','))]
     if not is_correctly_ordered(rules, update):
-        fixed = sorted(update, key=functools.cmp_to_key(cmp))
+        fixed = sorted(update, key=functools.cmp_to_key(cmp_with_rules(rules)))
         total += fixed[len(fixed) // 2]
 
 print(total)
